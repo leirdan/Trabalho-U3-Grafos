85051fe8a52c0f01717cdbd2e9e3acca8c24efca
Vinicius Sampaio <vldsampaio@pm.me>
2025-12-09 22:24:39 -0300

    feat: add standard deviation to the parse result script

--- parse_result.py
+++ parse_result.py
@@ -9,5 +9,7 @@ df = pd.read_csv(result, sep=" ", header=None)
 
 print("Min:", df[0].min())
 print("Avg cost:", df[0].mean())
+print("Standard deviation (cost):", df[0].std())
 print(f"Avg time: {df[1].mean()}s")
+print("Standard deviation (time):", df[1].std())
 print("Count:", len(df))
